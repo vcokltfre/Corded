0dda8b03c64485b6bbdd7e2af35f59442f12109f
vcokltfre <vcokltfre@gmail.com>
2021-03-22 13:42:11 +0000

    feat: add http errors

--- corded/errors.py
+++ corded/errors.py
@@ -1,2 +1,41 @@
+from aiohttp import ClientResponse
+
+
 class CordedError(Exception):
+    pass
+
+
+## HTTP Errors
+
+
+class HTTPError(CordedError):
+    def __init__(self, response: ClientResponse):
+        self.response = response
+
+
+class BadRequest(HTTPError):
+    pass
+
+
+class Unauthorized(HTTPError):
+    pass
+
+
+class Forbidden(HTTPError):
+    pass
+
+
+class NotFound(HTTPError):
+    pass
+
+
+class PayloadTooLarge(HTTPError):
+    pass
+
+
+class TooManyRequests(HTTPError):
+    pass
+
+
+class DiscordServerError(HTTPError):
     pass
