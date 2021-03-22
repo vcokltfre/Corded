5496ebd234146bad06fb294669c9ff25af312676
vcokltfre <vcokltfre@gmail.com>
2021-03-22 15:38:01 +0000

    feat: add in memory cache

--- corded/caching/cache.py
+++ corded/caching/cache.py
@@ -4,7 +4,7 @@ from typing import Union, List, Dict
 
 class BaseCache:
     @abstractmethod
-    async def set(key: str, value: str) -> int:
+    async def set(key: str, value: str) -> bool:
         pass
 
     @abstractmethod
@@ -20,5 +20,34 @@ class BaseCache:
         pass
 
     @abstractmethod
-    async def clear() -> int:
+    async def clear() -> bool:
         pass
+
+
+class MemoryCache(BaseCache):
+    def __init__(self, namespace: str = None):
+        """An im-memory cache for objects.
+
+        Args:
+            namespace (str, optional): The namespace to use for caching. Defaults to no namespace.
+        """
+
+        self.namespace = namespace or ""
+        self._cache = {}
+
+    def set(self, key: str, value: str) -> bool:
+        self._cache[key] = value
+        return 1
+
+    def get(self, key: str) -> Union[str, None]:
+        return self._cache.get(key)
+
+    def keys(self, pattern: str = None) -> List[str]:
+        if not pattern:
+            return list(self._cache.keys())
+        return [key for key in self._cache.keys() if pattern in key]
+
+    def items(self, pattern: str = None) -> Dict[str]:
+        if not pattern:
+            return self._cache
+        return dict((k, v) for k, v in self._cache.items() if pattern in k)
