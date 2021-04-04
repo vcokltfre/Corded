cd22af710a31afc80b4949412cddb8008bb45a6f
vcokltfre <vcokltfre@gmail.com>
2021-04-04 20:56:14 +0100

    feat: add base file with snowflake

---
+++ corded/objects/base.py
@@ -0,0 +1,50 @@
+"""
+MIT License
+
+Copyright (c) 2021 vcokltfre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from datetime import datetime
+
+
+class Snowflake:
+    def __init__(self, snowflake: int):
+        self.snowflake = snowflake
+
+        timestamp, worker, process, increment = self.deconstruct(snowflake)
+
+        self.timestamp = timestamp
+        self.worker = worker
+        self.process = process
+        self.increment = increment
+
+    @staticmethod
+    def deconstruct(snowflake: int):
+        timestamp = (snowflake >> 22) + 1420070400000
+        worker = (snowflake & 0x3E0000) >> 17
+        process = (snowflake & 0x1F000) >> 12
+        increment = snowflake & 0xFFF
+
+        return (timestamp, worker, process, increment)
+
+    @property
+    def isotime(self):
+        return datetime.fromtimestamp(self.timestamp).isoformat()
