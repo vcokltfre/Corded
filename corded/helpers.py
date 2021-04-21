57b2267d585dd885ff61761f5eb80eddad479c93
vcokltfre <vcokltfre@gmail.com>
2021-04-21 07:59:18 +0100

    feat: add bitfield helper

--- corded/helpers.py
+++ corded/helpers.py
@@ -37,3 +37,20 @@ def int_types(data: Union[dict, list, int, str, bool, float]):
         return {k: int_types(v) for k, v in data.items()}
     if isinstance(data, list):
         return [int_types(v) for v in data]
+
+
+class BitField:
+    def __init__(self, value: int):
+        self.value = value
+
+    def __getitem__(self, bit: int):
+        return self.value >> bit & 1
+
+    def __setitem__(self, bit: int, state: bool):
+        if state:
+            self.value |= (1<<bit)
+        else:
+            self.value &=~ (1<<bit)
+
+    def __int__(self):
+        return self.value
